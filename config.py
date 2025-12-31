ea105a26cfd47d05ce39c03e2f517e90149dcd89
karan-01 <karanvekariya90@gmail.com>
2025-12-31 11:20:00 +0530

    updated 31/12/25

---
+++ config.py
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "Agent API"
+    admin_email: str
+    database_url: str
+
+    class Config:
+        env_file = ".env"
+
+=======
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "Agent API"
+    admin_email: str
+    database_url: str
+
+    class Config:
+        env_file = ".env"
+
+>>>>>>> 6decf83 (Memory & Logging Layer)
+settings = Settings()
